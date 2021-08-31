03980dfae3d83279b664e0290554587e54430b09
bryhan alonzo <alonzo.code17@gmail.com>
2021-08-30 22:07:04 -0400

    initial commit

---
+++ balonzo_eat.py
@@ -0,0 +1,45 @@
+# Create resturant class
+
+class restaurant:
+    def __init__(self, menu, customers, order):
+        self.menu = menu 
+        self.customers = customers
+        self.order = order 
+
+
+# Create a  menu
+menu = [french_fries, sushi, steak, salad]
+
+# Create a customer
+
+# Create an order
+
+# create input
+
+
+# Menu: create a dictionary for menu
+
+# Menu: should have names and prices
+
+# MENU: add item to menu 
+
+# Menu: Should remove item from menu
+
+# Menu: item prices changed
+
+
+# Customer: name for cutomer
+
+# Customer: contact info phone or email
+
+# Customer: available funds so we can afford items.
+
+
+
+# Order: name for order
+
+# Order: items on the order
+
+# Order; status of the order
+
+# Order: checkout complete order based on customer info
